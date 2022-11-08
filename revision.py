b8fda6b46abf3a8b9acd50133ba4523f600fad1b
Fabrice Mehouelley <fabricemehouelley43@gmail.com>
2022-11-08 13:29:41 +0000

    j'apprend toujours  python c'est genial

---
+++ revision.py
@@ -0,0 +1,14 @@
+#coding:UTF-8
+nbre1= input("entrez nombre1")
+nbre2=input("entrez nombre 2")
+nom=input("entrez vore nom")
+"""
+Operation des nombre
+
+"""
+nbre1=int(nbre1)
+nbre2=int(nbre2)
+multi=nbre1*nbre2
+add=nbre1+nbre2
+print("{} la multiplication vaux {}".format(nom,multi))
+print("{} l'addition vaut ".format(nom),add)
--- srtucture.py
+++ srtucture.py
@@ -1,1 +1,25 @@
 #coding:UTF-8
+
+"""
+Operateur de comparaisons : == (egal à)
+                            != (différent de )
+                            <  (plus petit que)
+                            >   (plus grand que)
+                            <=   (plus petit ou égal à)
+                            >=   (plus grand ou égal à)
+
+Condition multiples       : and (ET)
+                            or (OU)
+                            in / not in (DANS PAS DANS)
+"""
+identifiant="Fabrice"
+mot_de_passe = "password1234"
+print("--------------Interface de connexion---------------")
+user_id=input("entrez votre identifiant : ")
+user_pass=input("entrez votre password : ")
+if(user_id==identifiant and user_pass==mot_de_passe):{
+    print("vous etes connecter bienvenue {}".format(user_id))
+}
+else:{
+ print("information incorect")
+}
