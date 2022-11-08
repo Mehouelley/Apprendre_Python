4ffe1bf0401de2e0863d4c6545294f279c7dd4bf
Fabrice Mehouelley <fabricemehouelley43@gmail.com>
2022-11-08 14:05:45 +0000

    j'apprend toujours  python c'est genial

--- srtucture.py
+++ srtucture.py
@@ -8,18 +8,45 @@ Operateur de comparaisons : == (egal à)
                             <=   (plus petit ou égal à)
                             >=   (plus grand ou égal à)
 
+Mots clés des conditions  : if / elif / else
+
 Condition multiples       : and (ET)
                             or (OU)
                             in / not in (DANS PAS DANS)
 """
 identifiant="Fabrice"
 mot_de_passe = "password1234"
+Age = input("entrez votre age :")
+Age = int(Age)
 print("--------------Interface de connexion---------------")
-user_id=input("entrez votre identifiant : ")
-user_pass=input("entrez votre password : ")
+
+#ser_id=input("entrez votre identifiant : ")
+#user_pass=input("entrez votre password : ")
+#letrre_hasard= input("entrez votre letrre : ")
+"""
 if(user_id==identifiant and user_pass==mot_de_passe):{
     print("vous etes connecter bienvenue {}".format(user_id))
 }
 else:{
  print("information incorect")
+}
+if(letrre_hasard in "aeiouy"):{
+    print(letrre_hasard," est un voyelle")
+}
+else:{
+
+    print(letrre_hasard," est un consomne")
+}
+"""
+if (Age > 0 and Age<18):{
+    print("tu es mineur")
+}
+elif(Age==18):{
+    print("tu es majeur")
+}
+elif(Age>18 and Age<=100):{
+    print("tu viellir")
+}
+else:{
+    print("lq mort te guette")
 }
